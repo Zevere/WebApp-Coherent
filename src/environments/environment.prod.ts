69350cff76ca1964170cbaa0fab1af38a49e7c91
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-10-12 11:54:21 -0400

    Update app settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { EnvSettings } from './env-settings';
 
 export const environment: EnvSettings = {
     production: true,
-    apiBaseUrl: 'https://zevere.herokuapp.com',
+    apiBaseUrl: 'https://zv-webapi-borzoo.herokuapp.com',
 
     tgBot: 'zvBot',
     tgRedirectUrl: 'https://zv-chatbot-hepir.herokuapp.com/api/login-widget'
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ import { EnvSettings } from './env-settings';
 
 export const environment: EnvSettings = {
     production: true,
-    apiBaseUrl: 'https://zevere-staging.herokuapp.com',
+    apiBaseUrl: 'https://zv-s-webapi-borzoo.herokuapp.com',
 
     tgBot: 'ZevereBot',
     tgRedirectUrl: 'https://zv-s-chatbot-hepir.herokuapp.com/api/login-widget'
