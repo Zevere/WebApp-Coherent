f1161b8cf087498acc82eb6e4dcb287017b7255c
Poulad Ashrafpour <poulad.pld@gmail.com>
2018-12-06 11:58:32 -0500

    Fix gh-pages deployment

--- src/environments/environment.staging.ts
+++
@@ -1,9 +0,0 @@
-import { EnvSettings } from './env-settings';
-
-export const environment: EnvSettings = {
-    production: true,
-    apiBaseUrl: 'https://zv-webapi-borzoo.herokuapp.com',
-
-    tgBot: 'ZevereBot',
-    tgRedirectUrl: 'https://zv-chatbot-hepir.herokuapp.com/api/login-widget'
-};
